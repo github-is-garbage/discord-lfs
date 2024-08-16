55d665b1fa7f7b9a4a977cdbcd6c05d06ee1e132
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-08-16 05:10:01 -0400

    Test dotenv 2

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+dotenv_location = find_dotenv()
 
-print(os.environ.get("BOT_TOKEN"))
+if len(dotenv_location) > 0:
+	load_dotenv(dotenv_location)
+
+	BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+	print(BOT_TOKEN)
+else:
+	print("Can't find .env - Aborting")
