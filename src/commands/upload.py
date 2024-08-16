eb601c5809422806a7c9afa685017799a2a027aa
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-08-16 06:38:08 -0400

    Temp help function

--- src/commands/upload.py
+++ src/commands/upload.py
@@ -1,9 +1,10 @@
 from bot import Bot
 import discord
+from temphelp import canthing
 
 @Bot.tree.command(name = "upload")
 async def upload(interaction: discord.Interaction, path: str):
-	if not interaction.user.guild_permissions.administrator: return await interaction.response.send_message("No")
-	if not interaction.user.resolved_permissions.administrator: return await interaction.response.send_message("No")
+	if not canthing(interaction): # TODO: Remove this
+		return await interaction.response.send_message("No")
 
 	await interaction.response.send_message(f"Pong! {path}")
